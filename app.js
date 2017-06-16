1df43bb07013e2e565836f89e208aef6ba9c33ee
amanda07511 <amanda07511@gmail.com>
2017-06-16 14:23:24 +0200

    [mod] adding Apidoc index like default page

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var cors	= require('cors');
 var path	= require('path');
 var opn 	= require('opn');
+var fs 		= require('fs');
+
 
 //Calling routes
 var user 		= require('./routes/user.js');
@@ -12,19 +14,19 @@ var annonce 	= require('./routes/annonce.js');
 var candidature = require('./routes/candidature.js');
 var rate 		= require('./routes/rate.js');
 
-
 //create application expres
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // views is directory for all template files
-app.set('views', __dirname + '/routes/doc');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.use(cors());
+app.use(express.static('views'));
 
 app.get('/',function(req,res){
-	res.send('Hello World!');
+	app.use(express.static(__dirname + 'views'));
+	res.render('index');
 });
 
 //Routes
---
+++ views/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Indaller",
+  "version": "0.1.0",
+  "description": "API for Indaller App",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-06-14T17:02:08.705Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
